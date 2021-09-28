25a5958b42ffc89b51b29c036b0b522a55266f51
Tobias Seeanner <webmaster.postsvlandshut@protonmail.com>
2021-09-28 13:19:38 +0200

    Spring Boot Setup for PCMS-Webapp
    Initialising of Spring Boot Framework with https://start.spring.io/
    Dependencies:
        >Spring Web
        >Thymeleaf
        >Spring Data JPA
        >Spring Security

---
+++ src/test/java/de/postsportvereinlandshut/PCMS/PcmsApplicationTests.java
@@ -0,0 +1,13 @@
+package de.postsportvereinlandshut.PCMS;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PcmsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
